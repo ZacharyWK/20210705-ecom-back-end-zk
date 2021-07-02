37be1eeda18625755b6653e4aa428a5711d9d70d
Zachary K <zachwkrause@gmail.com>
2021-07-02 10:54:57 -0700

    initial upload

---
+++ routes/api/product-routes.js
@@ -0,0 +1,145 @@
+// The `/api/products` endpoint
+
+const router = require('express').Router();
+const { Product, Category, Tag, ProductTag } = require('../../models');
+
+
+// Get all Products
+router.get('/', async (req, res) => {
+try
+{
+const productData = await Product
+.findAll(
+{
+include: [{ model: Category, model: Tag}],
+});
+if (!productData)
+{
+res.status(404).json
+({ message: 'No products found.'});
+return;
+}
+res.status(200).json(productData);
+}
+catch 
+(err) {res.status(500).json(err)}
+});
+
+
+// Get by Product ID
+router.get('/:id', async (req, res) => {
+try
+{
+const productData = await Product
+.findByPk(req.params.id, 
+{
+include: [{ model: Category, model: Tag}],
+});
+if (!productData) 
+{
+res.status(404).json({ message: 'Im afraid I cant let you do that, the ID is incorrect.'});
+return;
+}
+res.status(200).json(productData);
+} 
+catch
+(err) {res.status(500).json(err)}
+});
+
+
+// New Product -Code provided-
+router.post('/', (req, res) => {
+Product
+.create(req.body)
+.then((product) => {
+    // if there's product tags, we need to create pairings to bulk create 
+    // in the ProductTag model
+if (req.body.tagIds.length) 
+{
+const productTagIdArr = 
+req.body.tagIds.map((tag_id) => 
+{
+return {product_id: product.id,tag_id,};
+});
+return ProductTag.bulkCreate(productTagIdArr);
+}
+    // if no product tags, just respond
+res.status(200).json(product);
+})
+.then((productTagIds) => res.status(200).json(productTagIds))
+.catch((err) => {console.log(err);res.status(400).json(err);});
+});
+
+
+// Update Product -Code provided-
+router.put('/:id', (req, res) => {
+Product
+.update(req.body, 
+{
+where: 
+{id: req.params.id,},
+})
+.then((product) => {
+    // find all associated tags from ProductTag
+return ProductTag.findAll({ where: { product_id: req.params.id } });
+})
+.then((productTags) => {
+    // get list of current tag_ids
+const productTagIds = 
+productTags.map(({ tag_id }) => tag_id);
+    // create filtered list of new tag_ids
+const newProductTags = 
+req.body.tagIds
+.filter((tag_id) => !productTagIds.includes(tag_id))
+.map((tag_id) => {return {product_id: req.params.id,tag_id,};
+});
+    // figure out which ones to remove
+const productTagsToRemove = 
+productTags
+.filter(({ tag_id }) => !req.body.tagIds.includes(tag_id))
+.map(({ id }) => id);
+    // run both actions
+return Promise.all
+([
+ProductTag.destroy({ where: { id: productTagsToRemove } }),
+ProductTag.bulkCreate(newProductTags),
+]);
+})
+.then((updatedProductTags) => res.json(updatedProductTags))
+.catch((err) => {console.log(err);res.status(400).json(err);});
+});
+
+
+// Delete Product
+router.delete('/:id', async (req, res) => {
+try 
+{
+const productData =
+await Product.destroy({
+where:
+{id: req.params.id,}
+});
+if (!productData) 
+{
+res.status(404).json
+({ message: 'Im afraid I cant let you do that, the ID is incorrect.'});
+return;
+}
+res.status(200).json(productData);
+} 
+catch 
+(err) {res.status(500).json(err);}
+});
+
+
+module.exports = router;
+
+  
+/* req.body should look like this...
+    {
+      product_name: "Basketball",
+      price: 200.00,
+      stock: 3,
+      tagIds: [1, 2, 3, 4]
+    }
+  */
---
+++ routes/api/tag-routes.js
@@ -0,0 +1,115 @@
+// The `/api/tags` endpoint
+
+const router = require('express').Router();
+const { Tag, Product, ProductTag } = require('../../models');
+
+
+// Get all Tags
+router.get('/', async (req, res) => {
+try
+{
+const tagData = await Tag
+.findAll(
+{
+include: [{model: ProductTag, model: Product}],
+});
+if (!tagData)
+{
+res.status(404).json
+({ message: 'No tags found.'});
+return;
+}
+res.status(200).json(tagData);
+}
+catch 
+(err) {res.status(500).json(err)}
+});
+
+
+// Get by Tag ID
+router.get('/:id', async (req, res) => {
+try
+{
+const tagData = await Product
+.findByPk(req.params.id,
+{
+include: [{model: ProductTag, model: Product}],
+});
+if (!tagData)
+{
+res.status(404).json
+({ message: 'Im afraid I cant let you do that, the ID is incorrect.'});
+return;
+}
+res.status(200).json(tagData);
+} 
+catch
+(err) {res.status(500).json(err)}
+});
+
+
+// New Tag
+router.post('/', async (req, res) => {
+try
+{
+const tagData = 
+await Tag.create(req.body);
+if (!tagData)
+{
+res.status(404).json
+({ message: 'Im afraid I cant let you do that, the ID is incorrect.'});
+return;
+}
+res.status(200).json(tagData)
+}
+catch 
+(err) {res.status(500).json(err);}
+});
+
+
+// Update Tag
+router.put('/:id', async (req, res) => {
+try 
+{
+const tagData = 
+await Tag.update(req.body, 
+{
+where:
+{id: req.params.id,},
+});
+if (!tagData[0]) 
+{
+res.status(404).json
+({ message: 'Im afraid I cant let you do that, the ID is incorrect.'});
+return;
+}
+res.status(200).json(tagData);
+} 
+catch 
+(err) {res.status(500).json(err);}
+});
+
+
+// Delete Tag
+router.delete('/:id', async (req, res) => {
+try 
+{
+const tagData = 
+await Tag.destroy({
+where: 
+{id: req.params.id,},
+});
+if (!tagData) 
+{
+res.status(404).json
+({ message: 'Im afraid I cant let you do that, the ID is incorrect.'});
+return;
+}
+res.status(200).json(tagData);
+} 
+catch 
+(err) {res.status(500).json(err);}
+});
+
+
+module.exports = router;
